0ae627871878c3d16711a5a0b874f47a29ae11e5
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-07 19:22:30 +0300

    task2

---
+++ task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] FillRandomArray(int Arg)  //Создает произвольно заполненный массив
+{
+    int[] Array = new int[Arg];
+    for (int i = 0; i < Arg; i++)
+    {
+        Array[i] = new Random().Next(100, 1000);
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine("");
+    return Array;
+}
+
+int CountEven(int[] Array)  //Посчитывает количество четных чисел
+{
+    int Count = 0;
+    foreach (int Buf in Array)
+    {
+        if (Buf % 2 == 0) Count++;
+    }
+    return Count;
+}
+
+try
+{
+    Console.WriteLine("Введите размер массива");
+    int Size = Convert.ToInt32(Console.ReadLine());
+    int[] Array = new int[Size];
+    Array = FillRandomArray(Size);
+    int Result = CountEven(Array);
+    Console.WriteLine("Количество четных чисел в массиве: " + Result);
+}
+
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] FillRandomArray(int Arg)  //Создает произвольно заполненный массив
+{
+    int[] Array = new int[Arg];
+    for (int i = 0; i < Arg; i++)
+    {
+        Array[i] = new Random().Next(-10, 11);
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine("");
+    return Array;
+}
+
+int SumNotEven(int[] Array)  //Посчитывает сумму нечетных чисел
+{
+    int Sum = 0;
+    foreach (int Buf in Array)
+    {
+        if (Buf % 2 != 0) Sum += Buf;
+    }
+    return Sum;
+}
+
+try
+{
+    Console.WriteLine("Введите размер массива");
+    int Size = Convert.ToInt32(Console.ReadLine());
+    int[] Array = new int[Size];
+    Array = FillRandomArray(Size);
+    int Result = SumNotEven(Array);
+    Console.WriteLine("Количество четных чисел в массиве: " + Result);
+}
+
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
